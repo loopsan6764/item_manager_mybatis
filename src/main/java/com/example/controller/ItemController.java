f26273828b38df262eb6fc01e26ed62ce48b1c50
Kohki Homma <loop.skydriver@hotmail.co.jp>
2023-11-01 22:28:12 +0900

    課題2実装途中

--- src/main/java/com/example/controller/ItemController.java
+++ src/main/java/com/example/controller/ItemController.java
@@ -1,12 +1,31 @@
 package com.example.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.entity.Item;
+import com.example.service.ItemService;
 
 @Controller
 public class ItemController {
+    /* ☆追加箇所：Serviceクラス用のフィールドを追加する☆ */
+    private final ItemService itemService;
+
+   /* ☆追加箇所：Serviceクラスのインスタンスをフィールドにセットする☆ */
+    @Autowired
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
     @GetMapping("/index")
-    public String index() {
+    public String index(Model model) {
+        /* ☆修正箇所：商品の一覧を取得して、htmlで扱えるようにする☆ */
+        List<Item> items = this.itemService.findAll();
+        model.addAttribute("items", items);
         return "index";
     }
 }
